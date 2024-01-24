e58397c7825f214c2973db6ea1cb0c66cef54405
Fulano de Tal <dgabrielferrari7@gmail.com>
2024-01-23 21:39:39 -0300

    Calculadora.

--- carteira.js
+++ carteira.js
@@ -1,8 +1,43 @@
-let contador = 0
+let sim = "s"
 
-while (contador < 10){
-    console.log(contador)
-    contador++
+while (sim == "s"){
+    console.log("Operações disponiveis:\nDivisão /\nMultiplicação *\nAdição +\nSubtração -")
+    let operacao = prompt("Escolha uma operação")
+
+    if (operacao == "+"){
+        let x = Number(prompt("Digite um número inteiro:"))
+        console.log(x)
+        let y = Number(prompt("Digite mais um número inteiro:"))
+        console.log(y)
+        console.log(x + y)
+    }
+
+    else if (operacao == "-"){
+        let x = Number(prompt("Digite um número inteiro:"))
+        console.log(x)
+        let y = Number(prompt("Digite mais um número inteiro:"))
+        console.log(y)
+        console.log(x - y)
+    }
+
+    else if (operacao == "/"){
+        let x = Number(prompt("Digite um número inteiro:"))
+        console.log(x)
+        let y = Number(prompt("Digite mais um número inteiro:"))
+        console.log(y)
+        console.log(x / y)
+    }
+
+    else if (operacao == "*"){
+        let x = Number(prompt("Digite um número inteiro:"))
+        console.log(x)
+        let y = Number(prompt("Digite mais um número inteiro:"))
+        console.log(y)
+        console.log(x * y)
+    }
+    
+    sim = prompt("Deseja continuar? (s/n)")
+
 }
 
 console.log("Acabou!")
