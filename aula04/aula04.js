592ba3e2ecfc4aecc4f6ddfda74e3a76573d47e2
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-25 19:50:07 -0300

    Aula 4 Introdução.

---
+++ aula04/aula04.js
@@ -0,0 +1,6 @@
+let a = Math.random(); // Cria um número aleatório para avariável "a"/ Math é uma bibliotéca e "random()" é o método para criar um número aleatório
+
+console.log(a); // Printa o "a"
+console.log(a * 10); // Printa o número aleatório multiplicado por 10, pois o Math.random só cria números entre 0 e 1
+console.log(a * 10 + 1); // Printa o Math.randon() vezes dez e mais um paraque possa chegar a dez
+console.log(Math.floor(a * 10 + 1)); // Math.floor arredonda para baixo o número tirando o que vem depois da virgula
