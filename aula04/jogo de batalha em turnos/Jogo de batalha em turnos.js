3dbdc8e44ced0c37fc3247998e6a945255ba9c15
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-28 10:47:46 -0300

    Dobra Força por três rodadas okay.

--- aula04/jogo de batalha em turnos/Jogo de batalha em turnos.js
+++ aula04/jogo de batalha em turnos/Jogo de batalha em turnos.js
@@ -1,177 +1,223 @@
 let jogador_vida = 10;
 let inimigo_vida = 10;
 let recarregar_vida = 3;
-let dobrar_forca = 3;
-let contador_dobrar_forca = 0;
-let contador_rodadas = 0;
-while (jogador_vida > 0 && inimigo_vida > 0) {
-  contador_rodadas++;
-  var start = prompt(
-    `Rodada ${contador_rodadas}\nApert "enter" para iniciar a rodada`
-  );
-  //------------ // Contador Dobrar Força não pode estar ativado para este if // -----------//
-  if (start == "" && contador_dobrar_forca == 0) {
-    var sorteador = Math.floor(Math.random() * 10 + 1);
-    // ----------- Se o jogador for sorteado ------------//
-    if (sorteador <= 5) {
-      // --------- Se o jogador for sorteado e tiver a opção de dobrar força e de recarregar vida ----------//
-      if (recarregar_vida > 0 && jogador_vida <= 7 && dobrar_forca > 0) {
-        var opcao = prompt("Atacar = 1\nVida = 2\nDobrar-Ataque = 3");
-        //  --- Jogador Ataca --- //
-        if (opcao == 1) {
-          inimigo_vida--;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        //  --- Jogador Recarrega a Vida --- //
-        else if (opcao == 2) {
-          jogador_vida += 3;
-          recarregar_vida -= 1;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        // --- Jogador Dobra a Força --- //
-        else if (opcao == 3) {
-          inimigo_vida -= 2;
-          contador_dobrar_forca++; // Conta como primeira rodada do Dobra Força
-          dobrar_forca--; // Diminui 1
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-      }
-      // -------- Else if da opção Recarregar Vida e Atacar o Inimigo mas sem a opção Dobrar Força ------- //
-      else if (recarregar_vida > 0 && jogador_vida <= 7) {
-        var opcao = prompt("Atacar = 1\nVida = 2");
-        // --- Atacar Inimigo --- //
-        if (opcao == 1) {
-          inimigo_vida--;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        //  --- Jogador Recarrega a Vida --- //
-        else if (opcao == 2) {
-          jogador_vida += 3;
-          recarregar_vida -= 1;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-      }
-      // -------- Else if da opção Atacar o Inimigo e Dobrar Força mas sem a opção Recarregar Vida ------- //
-      else if (dobrar_forca > 0) {
-        var opcao = prompt("Atacar = 1\nDobrar-Ataque = 3");
-        // --- Atacar Inimigo --- //
-        if (opcao == 1) {
-          inimigo_vida--;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        // --- Jogador Dobra a Força --- //
-        else if (opcao == 3) {
-          inimigo_vida -= 2;
-          contador_dobrar_forca++; // Conta como primeira rodada do Dobra Força
-          dobrar_forca--; // Diminui 1
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-      }
-      // -- Else if de Atacar Inimigo sem opção de Dobrar Força e Recarregar Vida --- //
-      else {
-        inimigo_vida--;
-        alert(
-          `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-        );
-      }
-    }
-    // -------- Inimigo For Sorteado ele Ataca ------- //
-    else {
-      jogador_vida--;
-      alert(
-        `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-      );
-    }
+let df = 3;
+let rodadas = 0;
+let cdf = true;
+let cdf1 = false;
+let cdf2 = false;
+
+function sorteador() {
+  numero_sorteado = Math.floor(Math.random() * 10 + 1);
+  if (numero_sorteado <= 5) {
+    return true;
+  } else {
+    return false;
   }
-  // ------- // Contador Dobra força Ativado // ------ //
-  else if (start == "" && contador_dobrar_forca > 0) {
-    var sorteador = Math.floor(Math.random() * 10 + 1);
-    // ----------- Se o jogador for sorteado ------------//
-    if (sorteador <= 5) {
-      // ----- If da opção Recarregar Vida e Atacar o Inimigo e Contador Dobra Força na primeira rodada ---- //
-      if (
-        recarregar_vida > 0 &&
-        jogador_vida <= 7 &&
-        contador_dobrar_forca == 1
-      ) {
-        var opcao = prompt("Atacar = 1\nVida = 2");
-        // --- Jogador Ataca --- //
-        if (opcao == 1) {
-          inimigo_vida -= 2;
-          contador_dobrar_forca += 1;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        //  --- Jogador Recarrega a Vida --- //
-        else if (opcao == 2) {
-          jogador_vida += 3;
-          recarregar_vida -= 1;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-      }
-      // --- If da opção Atacar o Inimigo e Contador Dobra Força na primeira rodada mas sem Rearregar Vida -- //
-      else if (contador_dobrar_forca == 1) {
-        inimigo_vida -= 2;
-        contador_dobrar_forca += 1;
-        alert(
-          `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-        );
-      }
-      // ----- If da opção Recarregar Vida e Atacar o Inimigo e Contador Dobra Força na segunda rodada ---- //
-      else if (
-        recarregar_vida > 0 &&
-        jogador_vida <= 7 &&
-        contador_dobrar_forca == 2
-      ) {
-        var opcao = prompt("Atacar = 1\nVida = 2");
-        // --- Jogador Ataca --- //
-        if (opcao == 1) {
-          inimigo_vida -= 2;
-          contador_dobrar_forca = 0;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-        //  --- Jogador Recarrega a Vida --- //
-        else if (opcao == 2) {
-          jogador_vida += 3;
-          recarregar_vida -= 1;
-          alert(
-            `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-          );
-        }
-      }
-      // --- If da opção Atacar o Inimigo e Contador Dobra Força na segunda rodada mas sem Rearregar Vida -- //
-      else if (contador_dobrar_forca == 1) {
-        inimigo_vida -= 2;
-        contador_dobrar_forca = 0;
-        alert(
-          `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-        );
-      }
+}
+
+function confere_vida() {
+  if (recarregar_vida > 0 && jogador_vida <= 7) {
+    return true;
+  } else {
+    false;
+  }
+}
+
+function confere_start() {
+  start = prompt(`Rodada ${rodadas}\nApert "enter" para iniciar a rodada`);
+  return;
+}
+
+function cdf00() {
+  cdf = false;
+  cdf1 = true;
+  return;
+}
+
+function cdf01() {
+  cdf1 = false;
+  cdf2 = true;
+  return;
+}
+
+function cdf02() {
+  cdf2 = false;
+  cdf = true;
+  return;
+}
+
+function menu_cdf() {
+  if (numero_sorteado) {
+    if (vida && df > 0) {
+      menu01_cdf();
+    } else if (vida) {
+      menu02_cdf();
+    } else if (df > 0) {
+      menu03_cdf();
     } else {
-      jogador_vida--;
-      alert(
-        `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
-      );
+      menu04_cdf();
     }
+  } else {
+    inimigo_ataque();
+  }
+}
+
+function menu01_cdf() {
+  var opcao = Number(prompt("Atacar = 1\nVida = 2\nDobrar-Ataque = 3"));
+  if (opcao == 1) {
+    inimigo_vida--;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 2) {
+    jogador_vida += 3;
+    recarregar_vida -= 1;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 3) {
+    inimigo_vida -= 2;
+    cdf00();
+    df--;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  }
+}
+
+function menu02_cdf() {
+  var opcao = Number(prompt("Atacar = 1\nVida = 2"));
+  if (opcao == 1) {
+    inimigo_vida--;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 2) {
+    jogador_vida += 3;
+    recarregar_vida -= 1;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  }
+}
+
+function menu03_cdf() {
+  var opcao = prompt("Atacar = 1\nDobrar-Ataque = 3");
+  if (opcao == 1) {
+    inimigo_vida--;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 3) {
+    inimigo_vida -= 2;
+    cdf00();
+    df--;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  }
+}
+
+function menu04_cdf() {
+  inimigo_vida--;
+  return alert(
+    `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+  );
+}
+
+function inimigo_ataque() {
+  jogador_vida--;
+  return alert(
+    `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+  );
+}
+
+function menu_cdf1() {
+  if (numero_sorteado) {
+    if (vida) {
+      menu01_cdf1();
+    } else {
+      menu02_cdf1();
+    }
+  } else {
+    inimigo_ataque();
+  }
+}
+
+function menu01_cdf1() {
+  var opcao = prompt("Atacar = 1\nVida = 2");
+  if (opcao == 1) {
+    inimigo_vida -= 2;
+    cdf01();
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 2) {
+    jogador_vida += 3;
+    recarregar_vida -= 1;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  }
+}
+
+function menu02_cdf1() {
+  inimigo_vida -= 2;
+  cdf01();
+  return alert(
+    `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+  );
+}
+
+function menu_cdf2() {
+  if (numero_sorteado) {
+    if (vida) {
+      menu01_cdf2();
+    } else {
+      menu02_cdf2();
+    }
+  } else {
+    inimigo_ataque();
+  }
+}
+
+function menu01_cdf2() {
+  var opcao = prompt("Atacar = 1\nVida = 2");
+  if (opcao == 1) {
+    inimigo_vida -= 2;
+    cdf02();
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  } else if (opcao == 2) {
+    jogador_vida += 3;
+    recarregar_vida -= 1;
+    return alert(
+      `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+    );
+  }
+}
+
+function menu02_cdf2() {
+  inimigo_vida -= 2;
+  cdf02();
+  return alert(
+    `Vida do jogador: ${jogador_vida}\nVida do inimigo: ${inimigo_vida}`
+  );
+}
+
+while (jogador_vida > 0 && inimigo_vida > 0) {
+  rodadas++;
+  var numero_sorteado = sorteador();
+  var start = confere_start();
+  var vida = confere_vida();
+  if (cdf) {
+    menu_cdf();
+  } else if (cdf1) {
+    menu_cdf1();
+  } else if (cdf2) {
+    menu_cdf2();
   }
 }
 
