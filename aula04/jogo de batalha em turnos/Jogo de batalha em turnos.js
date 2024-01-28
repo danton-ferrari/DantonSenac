1004d91bf584145467602ab74ac9b47029d5f065
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-28 12:38:00 -0300

    Adicão de esperiencia e nivel okays.

--- aula04/jogo de batalha em turnos/Jogo de batalha em turnos.js
+++ aula04/jogo de batalha em turnos/Jogo de batalha em turnos.js
@@ -222,6 +222,11 @@ function menu02_cdf2() {
 }
 // ------------------------------ End Functions ------------------------------ //
 
+// ------------------------------ Start Variaveis ------------------------------ //
+let perdeu = false;
+let experiencia = 0;
+let contador_experiencia = 0;
+let nivel_jogador = 0;
 let jogador_vida = 10;
 let inimigo_vida = 10;
 let recarregar_vida = 3;
@@ -231,60 +236,151 @@ let cdf = true;
 let cdf1 = false;
 let cdf2 = false;
 let nivel_partida = Number(prompt("Easy = 1\nIntermediary = 2\nHard = 3:"));
+// ------------------------------ End Variaveis ------------------------------ //
 
 // --- Easy --- //
 if (nivel_partida == 1) {
-  while (jogador_vida > 0 && inimigo_vida > 0) {
-    rodadas++;
-    var numero_sorteado = sorteador_easy();
-    var start = confere_start();
-    var vida = confere_vida();
-    if (cdf) {
-      menu_cdf();
-    } else if (cdf1) {
-      menu_cdf1();
-    } else if (cdf2) {
-      menu_cdf2();
+  while (true) {
+    if (perdeu) {
+      break;
+    } else if (contador_experiencia < 3) {
+      while (jogador_vida > 0 && inimigo_vida > 0) {
+        rodadas++;
+        var numero_sorteado = sorteador_easy();
+        var start = confere_start();
+        var vida = confere_vida();
+        if (cdf) {
+          menu_cdf();
+        } else if (cdf1) {
+          menu_cdf1();
+        } else if (cdf2) {
+          menu_cdf2();
+        }
+      }
+      if (jogador_vida > 0) {
+        experiencia++;
+        contador_experiencia++;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else if (inimigo_vida > 0 && experiencia > 0) {
+        experiencia--;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else {
+        alert("Você perdeu!");
+        perdeu = true;
+        break;
+      }
+    } else {
+      nivel_jogador++;
+      contador_experiencia = 0;
+      alert(`Nivel do jogador é: ${nivel_jogador}`);
     }
   }
 }
 
 // --- Intermediary --- //
 else if (nivel_partida == 2) {
-  while (jogador_vida > 0 && inimigo_vida > 0) {
-    rodadas++;
-    var numero_sorteado = sorteador_intermediary();
-    var start = confere_start();
-    var vida = confere_vida();
-    if (cdf) {
-      menu_cdf();
-    } else if (cdf1) {
-      menu_cdf1();
-    } else if (cdf2) {
-      menu_cdf2();
+  while (true) {
+    if (perdeu) {
+      break;
+    } else if (contador_experiencia < 3) {
+      while (jogador_vida > 0 && inimigo_vida > 0) {
+        rodadas++;
+        var numero_sorteado = sorteador_intermediary();
+        var start = confere_start();
+        var vida = confere_vida();
+        if (cdf) {
+          menu_cdf();
+        } else if (cdf1) {
+          menu_cdf1();
+        } else if (cdf2) {
+          menu_cdf2();
+        }
+      }
+      if (jogador_vida > 0) {
+        experiencia++;
+        contador_experiencia++;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else if (inimigo_vida > 0 && experiencia > 0) {
+        experiencia--;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else {
+        alert("Você perdeu!");
+        perdeu = true;
+        break;
+      }
+    } else {
+      nivel_jogador++;
+      contador_experiencia = 0;
+      alert(`Nivel do jogador é: ${nivel_jogador}`);
     }
   }
 }
 
 // --- Hard --- //
 else if (nivel_partida == 3) {
-  while (jogador_vida > 0 && inimigo_vida > 0) {
-    rodadas++;
-    var numero_sorteado = sorteador_hard();
-    var start = confere_start();
-    var vida = confere_vida();
-    if (cdf) {
-      menu_cdf();
-    } else if (cdf1) {
-      menu_cdf1();
-    } else if (cdf2) {
-      menu_cdf2();
+  while (true) {
+    if (perdeu) {
+      break;
+    } else if (contador_experiencia < 3) {
+      while (jogador_vida > 0 && inimigo_vida > 0) {
+        rodadas++;
+        var numero_sorteado = sorteador_hard();
+        var start = confere_start();
+        var vida = confere_vida();
+        if (cdf) {
+          menu_cdf();
+        } else if (cdf1) {
+          menu_cdf1();
+        } else if (cdf2) {
+          menu_cdf2();
+        }
+      }
+      if (jogador_vida > 0) {
+        experiencia++;
+        contador_experiencia++;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else if (inimigo_vida > 0 && experiencia > 0) {
+        experiencia--;
+        jogador_vida = 10;
+        inimigo_vida = 10;
+        rodadas = 0;
+        cdf = true;
+        df = 3;
+        alert(`Experiencia do jogador é: ${experiencia}`);
+      } else {
+        alert("Você perdeu!");
+        perdeu = true;
+        break;
+      }
+    } else {
+      nivel_jogador++;
+      contador_experiencia = 0;
+      alert(`Nivel do jogador é: ${nivel_jogador}`);
     }
   }
-}
-
-if (jogador_vida > 0) {
-  alert("Winner");
-} else {
-  alert("Game Over");
 }
