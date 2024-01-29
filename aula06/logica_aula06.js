91735800fa6369014a94e6b01a0250c01a753f3f
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-29 19:52:57 -0300

    Exercicio de lógica 6 okay.

---
+++ aula06/logica_aula06.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt("Diga um número"));
+if (numero > 10) {
+  console.log("Aprovado");
+}
+
+// numero > 10 || numero % 2 == 1 --> Número maior que 10 ou Impar.
+// 0 < numero <= 10 --> Número menor ou igual a 10 e maior que 0.
+// 0 > numero || numero % 2 == 0 || numero == 10 --> Número negativo ou par ou igual a 0.
