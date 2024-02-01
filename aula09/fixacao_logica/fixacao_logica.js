f86d540edb3775a03f340e0c2a749f7bab5247a1
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 19:35:08 -0300

    Fixação de lógica.

---
+++ aula09/fixacao_logica/fixacao_logica.js
@@ -0,0 +1,12 @@
+let x = prompt("Responda");
+// 1- Se x>10 escreva maior que 10 se não menor que 10
+// alert(x >= 10 ? "Maior que 10" : "Menor que 10");
+// 2- Se x for o igual a “Marcelo”, escreva “esse cara mesmo”, se não
+// “não é esse cara”
+// alert(x == "Marcelo" ? "Esse cara mesmo" : "Não é esse cara");
+// 3- Se a segunda letra de uma frase for igual a “x”, escreva achei, se
+// não, “não achei”(se ficar confuso, lembre-se que no javascript frases
+// se comportam como listas quando queremos saber uma posição nelas)
+// alert(x[1] == "x" ? "Achei" : "Não achei");
+// 4- Se x for par, escreva “ganhei”, se não, escreva “perdi”
+// alert(x % 2 == 0 ? "Ganhei" : "Perdi");
