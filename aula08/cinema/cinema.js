1779ac4b432677b9cadfb86249b37046fdfac7af
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-01-31 21:28:54 -0300

    Inicio exercise cinema

---
+++ aula08/cinema/cinema.js
@@ -0,0 +1,8 @@
+// let minilista = [0,0,0,0,"\n"]
+// let lista = [minilista,minilista,minilista,minilista]
+// let mensagem = ""
+// for(let i=0; i<lista.length;i++){
+//   for(var j=0; j<minilista.length;j++ ){
+//   mensagem+=lista[i][j]
+//   }
+// }
