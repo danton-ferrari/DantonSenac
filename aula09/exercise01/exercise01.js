636eef6527c91cb744862e72fb44bd18aa634c4a
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 19:37:10 -0300

    Exercise 01.

---
+++ aula09/exercise01/exercise01.js
@@ -0,0 +1,5 @@
+// Exercício 1- Calculadora de desconto:
+// Imagine que você tenha que criar um software para uma loja que
+// sempre dá 5% de desconto para compras acima de R$100, monte
+// de tal forma que o caixa escreva o preço e depois informe quanto
+// que tem que pagar.
