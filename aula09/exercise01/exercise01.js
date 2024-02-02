bfdcead4ff9531b95d6d7656f55bafef7fc06f22
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 21:28:26 -0300

    Exercise 1 descontos 5% e 10% ok.

--- aula09/exercise01/exercise01.js
+++ aula09/exercise01/exercise01.js
@@ -3,6 +3,17 @@
 // sempre dá 5% de desconto para compras acima de R$100, monte
 // de tal forma que o caixa escreva o preço e depois informe quanto
 // que tem que pagar.
+// Exercício 1.2- Mais descontos:
+// Agora além de 5% em compras acima de R$100, também temos
+// 10% para compras acima de R$250.
+// Exercício 1.3- Continuar recebendo valores:
+// Faça o programa receber vários preços e somar eles, até digitar
+// algum valor que o faça sair do loop, e no final calcula se deu o
+// desconto ou não e mostra o preço a pagar na tela.
+// Exercício 1.4- Toques finais:
+// Usando uma lista, registre cada preço que foi escrito, e no final,
+// além de dizer o total a pagar, informe quais os subtotais (Valores
+// que foram adicionados).
 
 function menu(itens) {
   var qtde = Number(prompt("Digite a quantidade do produto:"));
@@ -47,8 +58,10 @@ while (true) {
     'Para adicionar mais alguma coisa aperte "+"\nPara emitir a NF aperte "-"'
   );
   if (continuar == "-") {
-    let desconto = preco * 0.05;
-    let preco_novo = preco >= 100 ? preco - desconto : preco;
+    let desconto10 = preco * 0.1;
+    let desconto05 = preco * 0.05;
+    let preco_novo =
+      preco >= 100 && preco < 250 ? preco - desconto05 : preco - desconto10;
     let preco_desconto =
       "\nO preço com desconto é de: R$" + preco_novo.toFixed(2);
     nota_fiscal.push(preco_desconto);
