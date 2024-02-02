8843a4b4ab9c830c313b77f016b44691617bfe38
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-02 19:34:25 -0300

    Exercise cinema okay.

--- aula08/cinema/cinema.js
+++ aula08/cinema/cinema.js
@@ -1,3 +1,8 @@
+// Exercício 2: Monte um algoritmo que te mostre quais acentos no
+// cinema estão ocupados, e usando um sistema estilo “batalha
+// naval” de posicionamento, informe qual acento ocupar, e qual
+// desocupar.
+
 // let minilista = [0,0,0,0,"\n"]
 // let lista = [minilista,minilista,minilista,minilista]
 // let mensagem = ""
@@ -7,13 +12,24 @@
 //   }
 // }
 
-let cadeiras1 = [0, 0, 0, 0, 0, "\n"];
-let cadeiras2 = [cadeiras1, cadeiras1, cadeiras1, cadeiras1, cadeiras1];
+let array = [
+  [0, 0, 0, 0, 0, "\n"],
+  [0, 0, 0, 0, 0, "\n"],
+  [0, 0, 0, 0, 0, "\n"],
+  [0, 0, 0, 0, 0, "\n"],
+  [0, 0, 0, 0, 0, "\n"],
+];
 
-alert(cadeiras2.join(""));
-var mensagem = Number(prompt("Qual cadeira você quer?"));
-for (let i = 0; i < lista.length; i++) {
-  for (var j = 0; j < minilista.length; j++) {
-    mensagem += lista[i][j];
+alert(array.join(""));
+let mostrar = "";
+var opcao = Number(prompt("Escolha fileira da cadeira:?"));
+var opcao2 = Number(prompt("Escolha a coluna da cadeira:"));
+if (array[opcao][opcao2] != "x") {
+  array[opcao][opcao2] = "x";
+}
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    mostrar += " " + array[i][j];
   }
 }
+alert(mostrar);
