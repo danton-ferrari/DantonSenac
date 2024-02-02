35ed9362c5d0ac3aff771a0d3df9205512dd14d3
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 21:02:14 -0300

    Cinema incompleyo.

--- aula08/cinema/cinema.js
+++ aula08/cinema/cinema.js
@@ -6,3 +6,14 @@
 //   mensagem+=lista[i][j]
 //   }
 // }
+
+let cadeiras1 = [0, 0, 0, 0, 0, "\n"];
+let cadeiras2 = [cadeiras1, cadeiras1, cadeiras1, cadeiras1, cadeiras1];
+
+alert(cadeiras2.join(""));
+var mensagem = Number(prompt("Qual cadeira você quer?"));
+for (let i = 0; i < lista.length; i++) {
+  for (var j = 0; j < minilista.length; j++) {
+    mensagem += lista[i][j];
+  }
+}
