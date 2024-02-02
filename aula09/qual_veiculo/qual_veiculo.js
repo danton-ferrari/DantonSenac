3b1fe2c5140c5d4962e5cd26be9c380216258c35
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 23:32:13 -0300

    Uso de Array dimensional para o exercise qual veiculo.

--- aula09/qual_veiculo/qual_veiculo.js
+++ aula09/qual_veiculo/qual_veiculo.js
@@ -12,3 +12,28 @@
 // Exercício 2.3- Descontos e acréscimos:
 // Se a distância for menor que 20Km, dê um desconto de 15%, caso
 // distância seja maior que 120Km, cobrar 15% a mais.
+
+let array = [
+  ["carro", 0, 500, 2],
+  ["van", 501, 2000, 2.5],
+  ["caminhão", 2001, 10000, 3.5],
+];
+
+let carga = Number(prompt("Qual a carga?"));
+let distancia = Number(prompt("Qual a distancia em Km?"));
+for (var i = 0; i < array.length; i++) {
+  if (carga >= array[i][1] && carga <= array[i][2]) {
+    var preco = carga * distancia * array[i][3];
+    var desconto_acrescimo =
+      distancia <= 20000
+        ? preco - preco * 0.15
+        : 20000 < distancia <= 120000
+        ? (desconto_acrescimo = preco)
+        : desconto_acrescimo + desconto_acrescimo * 0.15;
+    alert(
+      `Você precisa usar um(a) ${
+        array[i][0]
+      }\nO valor a pagar é de R$${desconto_acrescimo.toFixed(2)}`
+    );
+  }
+}
