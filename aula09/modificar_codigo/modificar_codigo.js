df787f7c4dc344398038153e613fadf69465df90
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-02 20:58:55 -0300

    Exercise Modificar Código okay.

--- aula09/modificar_codigo/modificar_codigo.js
+++ aula09/modificar_codigo/modificar_codigo.js
@@ -7,29 +7,52 @@
 // senha, se ele errar 5 vezes ele para.
 
 let usuarios = ["Anderson", "Julia", "Alister"];
+let usuarios_email = [
+  "Anderson@gmail.com",
+  "Julia@gmail.com",
+  "Alister@gmail.com",
+];
 let senhas = ["123", "meu nome e julha", "senh4 fort3"];
 while (true) {
-let novoUsuario = prompt("Insira um novo nome de usuário (ou 'sair'
-para encerrar):");
-if (novoUsuario.toLowerCase() == "sair") {
-break;
-}
-if (usuarios.indexOf(novoUsuario) == -1) {
-let novaSenha = prompt("Insira a senha correspondente:");
-usuarios.push(novoUsuario);
-senhas.push(novaSenha);
-alert(`Usuário "${novoUsuario}" cadastrado com sucesso.`);
-} else {
-alert(`Erro: O nome de usuário "${novoUsuario}" já está em uso.
+  let novoUsuario = prompt(
+    'Insira um novo nome de usuário (ou "sair" para encerrar):'
+  );
+  if (novoUsuario.toLowerCase() == "sair") {
+    break;
+  }
+  if (usuarios.indexOf(novoUsuario) == -1 && novoUsuario.indexOf("@") == -1) {
+    let novaSenha = prompt("Insira a senha correspondente:");
+    usuarios.push(novoUsuario);
+    senhas.push(novaSenha);
+    alert(`Usuário "${novoUsuario}" cadastrado com sucesso.`);
+  } else if (
+    usuarios_email.indexOf(novoUsuario) == -1 &&
+    novoUsuario.indexOf("@") != -1
+  ) {
+    let novaSenha = prompt("Insira a senha correspondente:");
+    usuarios_email.push(novoUsuario);
+    senhas.push(novaSenha);
+    alert(`Usuário "${novoUsuario}" cadastrado com sucesso.`);
+  } else {
+    alert(`Erro: O nome de usuário "${novoUsuario}" já está em uso.
 Escolha outro.`);
+  }
 }
+let chances = 0;
+while (chances < 5) {
+  let usuarioLogin = prompt("usuário:");
+  let senhaLogin = prompt("senha:");
+  let indiceUsuario =
+    usuarioLogin.indexOf("@") == -1
+      ? usuarios.indexOf(usuarioLogin)
+      : usuarios_email.indexOf(usuarioLogin);
+  if (indiceUsuario != -1 && senhas[indiceUsuario] == senhaLogin) {
+    alert(`Login bem-sucedido para o usuário "${usuarioLogin}".`);
+    break;
+  } else {
+    alert("Erro: Nome de usuário ou senha incorretos.");
+    chances++;
+  }
 }
-let usuarioLogin = prompt("usuário:");
-let senhaLogin = prompt("senha:");
-let indiceUsuario = usuarios.indexOf(usuarioLogin);
-if (indiceUsuario != -1 && senhas[indiceUsuario] == senhaLogin) {
-alert(`Login bem-sucedido para o usuário "${usuarioLogin}".`);
-} else {
-alert("Erro: Nome de usuário ou senha incorretos.");
-}
-console.log(usuarios)
+console.log(usuarios);
+console.log(usuarios_email);
