fc8ce191fb097b28478fe7cf4821363b5ffea5f6
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-01 21:31:35 -0300

    Exercise qual veiculo usar.

---
+++ aula09/qual_veiculo/qual_veiculo.js
@@ -0,0 +1,14 @@
+// Exercício 2- Qual veículo usar?:
+// Uma empresa de transporte de carga contratou você para criar um
+// programa que determina o tipo de veículo necessário para
+// transportar uma carga com base no peso. Se o peso for até 500 kg,
+// sugira um carro, se for até 2000 kg, sugira uma van, caso contrário,
+// sugira um caminhão.
+// Exercício 2.2- Preços:
+// Agora definido o veiculo, com base nele vamos calcular o valor do
+// frete, que deve ser o peso da carga vezes a distância percorrida
+// vezes 2 para carro, peso vezes distância vezes 2.5 para van, e peso
+// vezes distância vezes 3,5 para caminhão.
+// Exercício 2.3- Descontos e acréscimos:
+// Se a distância for menor que 20Km, dê um desconto de 15%, caso
+// distância seja maior que 120Km, cobrar 15% a mais.
