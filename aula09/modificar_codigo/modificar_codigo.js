6c56ff9b8c23d4bf7ed2be1cfc94fd9791b9bbf8
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-02 19:37:51 -0300

    Exercise Modificar Código.

---
+++ aula09/modificar_codigo/modificar_codigo.js
@@ -0,0 +1,7 @@
+// Exercício 3- Modificar código:
+// Com base no acima, crie uma lista para outro dado que o usuário
+// possa inserir, como email ou CPF.
+// Exercício 3.2- Faça com que ele possa usar tanto o nome quando
+// esse outro dado para logar.
+// Exercício 3.3- Faça com que ele possa tentar mais de uma vez por a
+// senha, se ele errar 5 vezes ele para.
