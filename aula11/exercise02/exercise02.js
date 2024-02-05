558a23c9fcdbd118c561fb4dc1b38a3054d2c191
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 20:46:11 -0300

    Aula 11, exercise 02 sobre switch.

---
+++ aula11/exercise02/exercise02.js
@@ -0,0 +1,3 @@
+// Exercício 2: Escreva um algoritmo onde para a resposta 1,2,3
+// responda “valores baixos” para 4,5,6 “valores médios” e 7,8,9
+// valores altos, no caso de outra coisa escreva “não registrado”.
