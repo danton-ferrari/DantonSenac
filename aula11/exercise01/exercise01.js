bbc17de96ca4f3c672f1265f7b3241fafcfc5f64
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 20:15:26 -0300

    Aula 11, exercise 01 de Switch.

---
+++ aula11/exercise01/exercise01.js
@@ -0,0 +1,5 @@
+// Exercício 1: Monte um esquema onde o programa receba um nome
+// de usuário, e se este nome for igual a “Maria” escreva “Pode
+// entrar”, se for “Jorge” escreva “Ainda falta pagar pra entrar”, se
+// estiver escrito “Claiton” mostre “Pq demorou tanto???” e caso
+// qualquer outra coisa escreva “Não sei”.
