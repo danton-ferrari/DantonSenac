9794e0ebac06e81eee05a129507e5f4b021e16bf
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 20:43:40 -0300

    Aula 11 exercise 01 sobre switch okays.

--- aula11/exercise01/exercise01.js
+++ aula11/exercise01/exercise01.js
@@ -3,3 +3,22 @@
 // entrar”, se for “Jorge” escreva “Ainda falta pagar pra entrar”, se
 // estiver escrito “Claiton” mostre “Pq demorou tanto???” e caso
 // qualquer outra coisa escreva “Não sei”.
+
+var nome = prompt("Digite seu nome:");
+switch (nome) {
+  case "Maria":
+    console.log("Pode entrar!");
+    break;
+
+  case "Jorge":
+    console.log("Ainda falta pagar pra entrar");
+    break;
+
+  case "Claiton":
+    console.log("Pq demorou tanto???");
+    break;
+
+  default:
+    console.log("Não sei.");
+    break;
+}
