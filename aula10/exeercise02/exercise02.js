3848c17d3479b72ae89fcca82b453beed06d0295
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 19:51:53 -0300

    Exercise 02 calculadora de ifs okay.

--- aula10/exeercise02/exercise02.js
+++ aula10/exeercise02/exercise02.js
@@ -2,3 +2,36 @@
 // Monte uma calculadora, assim como na primeira aula, (usando
 // sistema de ifs), mas dessa vez faça ele puxar uma função para
 // cada conta.
+
+// --- Start Fanctions -- //
+function divisao(a, b) {
+  console.log(a / b);
+}
+function multiplicacao(a, b) {
+  console.log(a * b);
+}
+function adicao(a, b) {
+  console.log(a + b);
+}
+function subtracao(a, b) {
+  console.log(a - b);
+}
+// --- Finish Fanctions -- //
+
+var operacao = prompt(
+  "Digite a operação desejada:\nDividido -> / \nMultiplicação -> *\nAdição -> +\nSubtração -> -"
+);
+var a = Number(prompt("Digite um número:"));
+var b = Number(prompt("Digite mais um número:"));
+if (operacao == "/") {
+  divisao(a, b);
+}
+if (operacao == "*") {
+  multiplicacao(a, b);
+}
+if (operacao == "+") {
+  adicao(a, b);
+}
+if (operacao == "-") {
+  subtracao(a, b);
+}
