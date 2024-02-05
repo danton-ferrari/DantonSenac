917389df415e8461e097afad5b555f456b9a8efb
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 19:54:23 -0300

    Exercise 03 Combinação de Códigos.

---
+++ aula10/exercise03/exercise03.js
@@ -0,0 +1,5 @@
+// Exercício 3-
+// Combine pelo menos 3 códigos que já tenha feito anteriormente,
+// pergunte o que deseja fazer desta vez, e ao selecionar o item,
+// realize ele, quando acabar volte para o início e pergunte
+// novamente.
