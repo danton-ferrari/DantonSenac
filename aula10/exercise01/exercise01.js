043d91e304e9a40fd1ee5fd8f44c63d40e4589f0
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 19:39:20 -0300

    Exercise 01 de saudação ou soma okay.

--- aula10/exercise01/exercise01.js
+++ aula10/exercise01/exercise01.js
@@ -2,3 +2,28 @@
 // Caso o usuário escreva “soma”, abra uma calculadora de soma,
 // caso escreva saudação, abra um gerador de saudação. (olhar
 // exemplos)
+
+// --- Start Functions --- //
+function soma(a, b) {
+  return a + b;
+}
+
+function saudacao(nome) {
+  console.log("Olá, " + nome + "!");
+}
+// --- Finish Functions --- //
+
+var nome = prompt("Digite o seu nome:");
+var qual_function = prompt(
+  'Digite "+" para somar e "s" para saudações:'
+).toLocaleLowerCase();
+if (qual_function == "s") {
+  saudacao(nome);
+} else if (qual_function == "+") {
+  a = Number(prompt("Digite um número:"));
+  b = Number(prompt("Digite mais um número:"));
+  resultado = soma(a, b);
+  console.log(resultado);
+} else {
+  console.log("Valor informado incorreto!");
+}
