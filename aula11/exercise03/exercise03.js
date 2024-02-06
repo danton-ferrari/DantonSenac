8af9a23ffb40f93f36d03b63c0f1e7e6cd9d5a3c
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-05 21:20:14 -0300

    Aula 11, exercise 03, jogo de perguntas okay.

--- aula11/exercise03/exercise03.js
+++ aula11/exercise03/exercise03.js
@@ -1,3 +1,43 @@
 // Exercício 3: relembrando a primeira aula, faça um jogo de múltipla
 // escolha que contenham uma história, se quiser usar funções para
 // separar as perguntas sinta-se livre.
+
+// --- Start Functions --- //
+function a(nome) {
+  console.log(`${nome}, você mora a 30min de carro e, está a pé. Cagou-se...`);
+}
+function b(nome) {
+  console.log(`${nome}, você mora a 30min de carro e, está a pé. Cagou-se...`);
+}
+function c(nome) {
+  console.log(`${nome}, você mora a 30min de carro e, está a pé. Cagou-se...`);
+}
+// --- Finish Functions --- //
+var continuar = true;
+while (continuar) {
+  let nome = prompt("Qual o seu nome?");
+  let variavel = `${nome} você está indo para casa de um amigo, mas precisa ir ao banheiro\no que faze?\na - voltar para casa\nb - seguir em frente\nc - procurar um estabelecimento`;
+  let decisao = prompt(variavel);
+  switch (decisao) {
+    case "a":
+      a(nome);
+      break;
+    case "b":
+      b(nome);
+      break;
+    case "c":
+      c(nome);
+      break;
+    default:
+      console.log(nome, ", valor invalido, digite novamente.");
+      break;
+  }
+  continuar = prompt("Deseja continuar a jogar? (s/n)");
+  switch (continuar) {
+    case "n":
+      continuar = false;
+      break;
+    default:
+      break;
+  }
+}
