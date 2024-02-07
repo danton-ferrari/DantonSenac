74386f812d3659adad6982f67f5f7c055fff1ef4
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-06 21:04:57 -0300

    Add função de adicionar nome e senha e função de remover nome e senha do exercise 01 da aula 12. E editando pastas da aula 06.

--- aula12/exercise01/exercise01.js
+++ aula12/exercise01/exercise01.js
@@ -6,14 +6,18 @@
 // ), e use o comando splice para apagar este, caso tenhamos um
 // mostrar, apenas escreva os valores em um alert. e caso modificar,
 // escreva o nome que quer ser alterado, o index dele e oque alterar
-
+contas[2].splice(0, 1);
+// --- Contas --- //
 let Contas = [
   ["Mario", "123", 99],
   ["Alfredo", "senha incrivel", 10000],
   ["Anderson", "acorda santacruz", 2000],
   ["Beatriz", "321", 53000],
 ];
-function nomePorSenha(contas, nome, senha) {
+// --- Contas --- //
+
+// --- Start Functions --- //
+function verificador(contas, nome, senha) {
   for (let i = 0; i < contas.length; i++) {
     if (contas[i][0] == nome) {
       if (contas[i][1] == senha) {
@@ -23,7 +27,59 @@ function nomePorSenha(contas, nome, senha) {
   }
   return false;
 }
-let nome = prompt("Diga um nome");
-let senhaProcurada = prompt("Digite uma senha");
-let encontrado = nomePorSenha(Contas, nome, senhaProcurada);
-console.log(encontrado);
+
+function adicionar(nome, senha) {
+  var temp = [nome, senha];
+  return temp;
+}
+function remover(Contas, nome) {
+  for (var i = 0; i < Contas.length; i++) {
+    if (Contas[i][0] == nome) {
+      return Contas[i].splice(0, 2);
+    }
+  }
+}
+function mostrar() {}
+function modificar() {}
+// --- Finish Functions --- //
+
+var continuar = true;
+
+while (continuar) {
+  var opcao = Number(
+    prompt(
+      "1 -> Adicionar nome a lista;\n2 -> Remover nome da lista;\n3 -> Mostrar nomes;\n4 -> Modificar nome."
+    )
+  );
+  let nome = prompt("Diga um nome");
+  let senha = prompt("Digite uma senha");
+  let encontrado = verificador(Contas, nome, senha);
+  console.log(encontrado);
+
+  switch (opcao) {
+    case 1:
+      if (encontrado != true) {
+        Contas.push(adicionar(nome, senha));
+        console.log(Contas);
+        alert(Contas);
+      } else {
+        console.log(`O nome ${nome} e/ou senha ${senha} já existe(m).`);
+      }
+      break;
+
+    case 2:
+      if (encontrado != true) {
+        Contas = remover(Contas, nome);
+      }
+      break;
+
+    case 3:
+      break;
+
+    case 4:
+      break;
+
+    default:
+      break;
+  }
+}
