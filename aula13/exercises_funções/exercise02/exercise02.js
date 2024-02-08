37a4bd67eb3cb15dc7f3d10a6482a5826aad87f8
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-07 21:07:17 -0300

    Exercise 03 de funções da aula 13 okay.

--- aula13/exercises_funções/exercise02/exercise02.js
+++ aula13/exercises_funções/exercise02/exercise02.js
@@ -3,13 +3,22 @@
 // segundo case crie uma função com os nomes destes itens de lista
 // criados anteriormente, mas que aleatoriamente escolha um deles.
 
-let opcao = prompt("Pergunta");
+function tela2() {
+  var lista = ["Maçã", "Laranja", "Uva", "Café", "Leite", "Pão"];
+  alert(lista[Math.floor(Math.random() * lista.length)]);
+}
+
+let opcao = prompt("Digite uma opção:\na\nb");
 switch (opcao) {
   case "a":
-    alert("primeira opção");
+    var numero = 10;
+    for (var i = 0; numero > 0; i++) {
+      alert(numero);
+      numero--;
+    }
     break;
   case "b":
-    alert("segunda opção");
+    tela2();
     break;
   default:
     alert("nenhuma obtida");
