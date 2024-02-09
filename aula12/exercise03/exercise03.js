42ab7c215883cf1e62b357f976396b737153a708
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-08 21:14:44 -0300

    Aula 12 exercise 03 add style.css.

--- aula12/exercise03/exercise03.js
+++ aula12/exercise03/exercise03.js
@@ -1,3 +1,3 @@
 // Exercício 3:
 // Proponha um programa, desenhe o esquema dele usando a conta
-// do LucidChart que você criou, e explique a ideia depois.
+// do  LucidChart  que você criou, e explique a ideia depois.
