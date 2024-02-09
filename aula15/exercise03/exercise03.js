4dab8707e834aae091b922b4b2190a093d8abd4c
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-09 20:24:46 -0300

    Exercise 03 da aula 15 okay.

--- aula15/exercise03/exercise03.js
+++ aula15/exercise03/exercise03.js
@@ -1,3 +1,30 @@
 // Exercício 3:
 // Crie uma calculadora de área do cilindro, e pergunte para o usuário se ele quer calcular
 // área do círculo ou do cilindro.
+
+var continuar = true;
+while (continuar) {
+  var opcao = Number(
+    prompt("Digite qual áre quer calcular:\n1 -> Circulo\n2 -> Cilindro")
+  );
+  switch (opcao) {
+    case 1:
+      var raio = Number(prompt("Digite o raio:"));
+      var resultado = raio * raio;
+      alert((3.14 * resultado).toFixed(2));
+      break;
+    case 2:
+      var raio = Number(prompt("Digite o raio:"));
+      var h = Number(prompt("Digite a altura:"));
+      var resultado = 2 * 3.14 * raio * (raio + h);
+      alert(resultado.toFixed(2));
+      break;
+  }
+  continuar =
+    prompt('Digite "c" para continuar ou "s" para sair:').toLocaleLowerCase() ==
+    "c"
+      ? true
+      : false;
+}
+
+alert('Obrigado por usar "Calculadora Danton".');
