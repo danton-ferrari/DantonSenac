cd2e22e25d2c20c9b5e0962d11beb77d327793d4
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-09 19:54:49 -0300

    Exercise 01-2 aula 15 okay.

--- aula15/exercise01/exercise01.js
+++ aula15/exercise01/exercise01.js
@@ -1,30 +1,47 @@
 // Exercício 1:
 // 1- A partir deste de cima, que conta o número de vagais em uma palavra ou frase, crie
 // um que encontre letras contidas no nome Bruno, ou seja, onde tem as letras b, r, u, n, o.
+
+// let palavra = prompt("Digite uma palavra:").toLowerCase();
+// let contadorVogais = 0;
+// for (let i = 0; i < palavra.length; i++) {
+//   let letra = palavra.charAt(i);
+//   if (
+//     letra == "b" ||
+//     letra == "r" ||
+//     letra == "u" ||
+//     letra == "n" ||
+//     letra == "o"
+//   ) {
+//     contadorVogais++;
+//   }
+// }
+// alert(
+//   "A palavra " +
+//     palavra +
+//     " possui " +
+//     contadorVogais +
+//     " letras iguais a bruno."
+// );
+
 // 2- Faça outro que apague todas as vogais da frase, para isso, ao inves de ter um
 // contador de vogais, crie uma variável que começe vazia, com “”, e ao invés de analizar
 // se a letra é uma vogal, analise se ela não é, se não for, adicione ela à variável nova
 // criada agora.
-// 3- Caso a letra “a” aparece, escreva ela “o” no lugar dela
 
 let palavra = prompt("Digite uma palavra:").toLowerCase();
-let contadorVogais = 0;
 for (let i = 0; i < palavra.length; i++) {
   let letra = palavra.charAt(i);
   if (
-    letra == "b" ||
-    letra == "r" ||
-    letra == "u" ||
-    letra == "n" ||
-    letra == "o"
+    letra == "a" ||
+    letra == "e" ||
+    letra == "i" ||
+    letra == "o" ||
+    letra == "u"
   ) {
-    contadorVogais++;
+    palavra = palavra.replace(letra, "");
   }
 }
-alert(
-  "A palavra " +
-    palavra +
-    " possui " +
-    contadorVogais +
-    " letras iguais a bruno."
-);
+alert(palavra);
+
+// 3- Caso a letra “a” aparece, escreva ela “o” no lugar dela
