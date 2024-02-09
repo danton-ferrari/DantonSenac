790a2c6c1237f4a7aef8e9a467da0ad2a407357d
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-09 20:04:56 -0300

    Exercise 02 da aula 15 okay.

--- aula15/exercise02/exercise02.js
+++ aula15/exercise02/exercise02.js
@@ -1,2 +1,12 @@
 // Exercício 2:
 // Crie uma calculadora de área de círculo. Se não sabe o calculo pesquise.
+
+let sim = "s";
+
+while (sim == "s") {
+  let raio = Number(prompt("Digite o raio:"));
+  var resultado = raio * raio;
+  alert((3.14 * resultado).toFixed(2));
+}
+
+console.log('Obrigado por usar "Calculadora Danton".');
