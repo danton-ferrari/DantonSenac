968663ee3d7368e92b3c5cd84f12a9ee25332902
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-14 20:25:31 -0300

    Já fiz o exercise 2 da aula 16 no exercise 1.

--- aula16/exercise2/exercise2.js
+++ aula16/exercise2/exercise2.js
@@ -1,3 +1,46 @@
 // Exercício 2: Agora, ao invés deles serem variáveis, faça eles serem
 // posições numa lista, para que assim nós possamos só chamar uma
 // posição e teremos os itens.
+
+class Aluno {
+  constructor(nome, idade, turma) {
+    this.nome = nome;
+    this.idade = idade;
+    this.turma = turma;
+    this.aprovado = false;
+  }
+  setAprovado(aprovado) {
+    this.aprovado = aprovado;
+  }
+  getInfo() {
+    return `${this.nome}, ${this.idade} anos, Turma: ${this.turma}, Aprovado: ${
+      this.aprovado ? "Sim" : "Não"
+    }`;
+  }
+}
+//Dois exemplos de como criar
+let lista_alunos = [
+  new Aluno("Danton", 20, "9A"),
+  new Aluno("Marjory", 18, "9B"),
+  new Aluno("Thaís", 12, "9A"),
+  new Aluno("Fernanda", 16, "9B"),
+  new Aluno("Gabriela", 18, "9A"),
+  new Aluno("Jerri", 20, "9B"),
+  new Aluno("Djanice", 20, "9A"),
+  new Aluno("Jairo", 20, "9B"),
+  new Aluno("Thalis", 12, "9A"),
+  new Aluno("Silvania", 13, "9B"),
+  new Aluno("Alberi", 14, "9A"),
+  new Aluno("Diessi", 11, "9B"),
+  new Aluno("Luana", 19, "9A"),
+  new Aluno("Vó", 30, "9B"),
+  new Aluno("Tia", 20, "9A"),
+];
+for (var i = 0; i < lista_alunos.length; i++) {
+  if (i % 2 === 0) {
+    lista_alunos[i].setAprovado(true);
+  } else {
+    lista_alunos[i].setAprovado(false);
+  }
+  console.log(lista_alunos[i].getInfo());
+}
