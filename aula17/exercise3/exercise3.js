7147e937668ca4efca7efa0d06ed22fd1ed73601
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-15 20:36:01 -0300

    Exercise 3 da aula 17 está okay.

--- aula17/exercise3/exercise3.js
+++ aula17/exercise3/exercise3.js
@@ -2,3 +2,25 @@
 // pergunta, onde te mostra um prompt na tela e se a tua resposta for a
 // correta ela se autodestrói (basta só transformar todos seus dados em
 // null).
+
+function autoDestruir() {
+  esfinge = null;
+  return alert("Esfinge vai autodestruir");
+}
+
+let esfinge = {
+  resposta_correta: "10",
+  pergunta: function (resposta) {
+    if (resposta === this.resposta_correta) {
+      this.resposta_correta = null;
+      this.pergunta = null;
+      alert("Você acertou!");
+      autoDestruir();
+    } else {
+      alert("Você errou!");
+    }
+  },
+};
+
+var resposta = esfinge.pergunta(prompt("Quanto é 2x5:"));
+alert(esfinge);
