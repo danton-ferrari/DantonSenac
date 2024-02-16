673aa1914866cda52d0c559b75df21e3daf6fe6c
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-16 19:13:10 -0300

    Exercise 5 da aula 17 está okay.

--- aula17/exercise5/exercise5.js
+++ aula17/exercise5/exercise5.js
@@ -1,2 +1,48 @@
 // Exercício 5: No mesmo código, escreva todos os elementos contidos
 // nesse objeto.
+
+let pessoa = {
+  nome: "Danton",
+  idade: 20,
+  cor: "branco",
+};
+let carro = {
+  modelo: "Honda-Civic",
+  marca: "Honda",
+  cor: "branco",
+};
+let curso = {
+  nome: "Engenharia-Software",
+  duracao: 5,
+  tipo: "baicharel",
+};
+let comida = {
+  nome: "pizza",
+  tamanho: "familia",
+  sabor: "strogonoff",
+};
+let bebida = {
+  nome: "energetico",
+  marca: "Monster",
+  sabor: "laranja",
+};
+
+function editor(x) {}
+
+let objetos = [pessoa, carro, curso, comida, bebida];
+
+let pergunta = prompt(
+  "Digite uma palavra chave para iniciar a pesquisa:\nNome\nIdade\nCor\nDuracao\nTipo\nTamanho\nSabor\nMarca\nModelo"
+).toLocaleLowerCase();
+
+for (var objeto of objetos) {
+  var m = "";
+  if (objeto.hasOwnProperty(pergunta)) {
+    var um = Object.keys(objeto);
+    var dois = Object.values(objeto);
+    for (var i = 0; i < um.length; i++) {
+      m += um[i] + " = " + dois[i] + "\n";
+    }
+    alert(m);
+  }
+}
