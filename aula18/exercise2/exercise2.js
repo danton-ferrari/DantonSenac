0572c2879c5abcf5bfd6420e84aa0c8b9dd90d67
DANTON GABRIEL NEIS FERRARI <dgabrielferrari7@gmail.com>
2024-02-16 20:53:55 -0300

    Exercise 2 da aula 18 está okay.

--- aula18/exercise2/exercise2.js
+++ aula18/exercise2/exercise2.js
@@ -2,3 +2,12 @@
 // Sabendo agora como o throw funciona, pergunte para o usuário em um prompt se ele
 // deseja a opção número 1 ou a opção número 2, qualquer outro caso mande o erro,
 // “resposta inválida”.
+
+var opcao = Number(prompt("Digite 1 ou 2:"));
+try {
+  if (opcao != 1 || opcao != 2) {
+    throw new Error("Valor inválido!");
+  }
+} catch (error) {
+  console.log(error.message);
+}
